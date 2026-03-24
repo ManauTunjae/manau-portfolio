610b83cc1c42d4ec0bf33951553e3ee7e863ef49
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-24 14:51:44 +0100

    feat: create ScrollToTop.jsx snd modile.css

---
+++ components/Scroll/ScrollToTop.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { IoArrowUpCircle } from 'react-icons/io5';
+import { Link } from 'react-scroll';
+import styles from './ScrollToTop.module.css';
+
+const ScrollToTop = () => {
+    
+}
+
+return (
+    <div>
+
+    </div>
+)
+
+export default ScrollToTop;
