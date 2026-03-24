30e6af4dd1b1609215c2f7514fa281126bc186b2
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-24 15:09:45 +0100

    feat: add scroll up to intro page with animation icon and styling

--- components/Scroll/ScrollToTop.jsx
+++ components/Scroll/ScrollToTop.jsx
@@ -1,24 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { IoArrowUpCircle } from 'react-icons/io5';
-import { Link } from 'react-scroll';
-import styles from './ScrollToTop.module.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+import { IoChevronUp } from "react-icons/io5";
+import styles from "./ScrollToTop.module.css";
 
 const ScrollToTop = () => {
-    const [ isVaisible, setVisible ] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
+  useEffect(() => {
     const toggleVisibility = () => {
-        if (window.pageYOffset > 300) {
-            setVisible(true)
-        } else {
-            setVisible(false)
-        }
-    }
-}
+      // Visas efter 200px scroll
+      if (window.pageYOffset > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-return (
-    <div>
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
+  return (
+    <div
+      className={`${styles.scrollWrapper} ${isVisible ? styles.visible : styles.hidden}`}
+    >
+      <Link
+        to="intro"
+        smooth={true}
+        duration={800}
+        className={styles.scrollLink}
+      >
+        <div className={styles.iconContainer}>
+          <IoChevronUp className={styles.arrowIcon} />
+          <span className={styles.pulseRing}></span>
+        </div>
+      </Link>
     </div>
-)
+  );
+};
 
 export default ScrollToTop;
