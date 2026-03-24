aa39cdf5dd45bf923cdf59e59f9ca877f0176625
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-24 14:55:04 +0100

    feat: create condition if display is scrolled down to 300 px

--- components/Scroll/ScrollToTop.jsx
+++ components/Scroll/ScrollToTop.jsx
@@ -4,7 +4,15 @@ import { Link } from 'react-scroll';
 import styles from './ScrollToTop.module.css';
 
 const ScrollToTop = () => {
-    
+    const [ isVaisible, setVisible ] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setVisible(true)
+        } else {
+            setVisible(false)
+        }
+    }
 }
 
 return (
