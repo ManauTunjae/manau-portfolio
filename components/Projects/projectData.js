c5bc0ba2ad51340d0f4a7b5de07490689b2edc39
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-25 12:30:30 +0100

    feat: add a project into project dara

--- components/Projects/projectData.js
+++ components/Projects/projectData.js
@@ -1,47 +1,62 @@
 export const myProjects = [
   {
     id: 1,
-    title: "Dynamic Blog Platform",
-    description: "A dynamic blog platform featuring interactive posts. Demonstrates array data handling, dynamic DOM rendering, and a fully responsive layout.",
-    tags: ["JavaScript", "HTML5", "CSS3"],
-    github: "https://github.com/ManauTunjae/blog-platform",
-    live: "https://easyblogg.netlify.app",
-    image: "/images/image-blog.png"
+    title: "mini-applicant tracking system",
+    description:
+      "Mini-ATS is a modern, full-stack application built to simplify the recruiting process. With a focus on speed, AI insights, and a clean user interface, it helps recruiters manage job postings and candidates efficiently.",
+    tags: ["React", "Vite", "JavaScript", "CSS3"],
+    github: "https://github.com/ManauTunjae/mini-applicant-tracking-system", // Din länk
+    live: "https://mini-ats.netlify.app", // Din live-länk
+    image: "/images/Image-ats.png",
   },
   {
     id: 2,
     title: "Pokemon Application",
-    description: "An interactive React app using the PokéAPI. Users can browse the original 151 Pokémon and view detailed statistics and data.",
+    description:
+      "An interactive React app using the PokéAPI. Users can browse the original 151 Pokémon and view detailed statistics and data.",
     tags: ["React", "Vite", "JavaScript", "CSS3"],
     github: "https://github.com/ManauTunjae/react-pokemon-app.git",
     live: "https://zippy-stroopwafel-04bc8a.netlify.app",
-    image: "/images/image-pokemon.png"
+    image: "/images/image-pokemon.png",
   },
   {
     id: 3,
     title: "Quiz Application",
-    description: "A category-based quiz app focusing on JSON data management and DOM manipulation to create a seamless user experience.",
+    description:
+      "A category-based quiz app focusing on JSON data management and DOM manipulation to create a seamless user experience.",
     tags: ["JavaScript", "HTML5", "CSS3", "JSON"],
     github: "https://github.com/ManauTunjae/quizApplikation",
     live: "https://quizappllication.netlify.app",
-    image: "/images/image-quiz.png"
+    image: "/images/image-quiz.png",
   },
   {
     id: 4,
     title: "Rhythm API",
-    description: "A robust REST API for music management. Features secure authentication with JWT and full MongoDB database integration.",
+    description:
+      "A robust REST API for music management. Features secure authentication with JWT and full MongoDB database integration.",
     tags: ["Node.js", "Express", "MongoDB", "Postman"],
     github: "https://github.com/ManauTunjae/rhythm-api.git",
     type: "Backend",
-    image: "/images/image-rhythm.png"
+    image: "/images/image-rhythm.png",
   },
   {
-  id: 5,
-  title: "Professional Portfolio",
-  description: "My personal developer portfolio built with React. Features dynamic project rendering, custom CSS modules for a 'code-editor' aesthetic, and a fully responsive layout.",
-  tags: ["React", "Vite", "JavaScript", "CSS3"],
-  github: "https://github.com/ManauTunjae/manau-portfolio", // Din länk
-  live: "https://manau-portfolio.netlify.app", // Din live-länk
-  image: "/images/image-portfolio.png" 
-}
+    id: 5,
+    title: "Professional Portfolio",
+    description:
+      "My personal developer portfolio built with React. Features dynamic project rendering, custom CSS modules for a 'code-editor' aesthetic, and a fully responsive layout.",
+    tags: ["React", "Vite", "JavaScript", "CSS3"],
+    github: "https://github.com/ManauTunjae/manau-portfolio", // Din länk
+    live: "https://manau-portfolio.netlify.app", // Din live-länk
+    image: "/images/image-portfolio.png",
+  },
+  {
+    id: 6,
+    title: "Dynamic Blog Platform",
+    description:
+      "A dynamic blog platform featuring interactive posts. Demonstrates array data handling, dynamic DOM rendering, and a fully responsive layout.",
+    tags: ["JavaScript", "HTML5", "CSS3"],
+    github: "https://github.com/ManauTunjae/blog-platform",
+    live: "https://easyblogg.netlify.app",
+    image: "/images/image-blog.png",
+  },
 ];
