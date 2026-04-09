ddf90ee8745640aad5f6f225c64384f2a9464d3e
ManauTunjae <manau.tunjae@hotmail.com>
2026-04-09 21:35:07 +0200

    update navbar for mobile version

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,28 +1,48 @@
-import styles from './Navbar.module.css';
-import { Link } from 'react-scroll';
-import { IoMenu } from "react-icons/io5";
-import { useState } from 'react';
-
+import styles from "./Navbar.module.css";
+import { Link } from "react-scroll";
+import { IoMenu, IoClose } from "react-icons/io5"; // Lade till stäng-ikon
+import { useState } from "react";
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(true);
+  // Menyn ska vara stängd från början på mobilen
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-    return (
+
+  // Stäng menyn när man klickar på en länk
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
     <nav className={styles.navWrapper}>
-        <p className={styles.navBarName}>Manau Tunjae portfolio</p>
-        <div className={`styles.navBarContainer ${isOpen ? styles.navBarContainer : ''}`}>
-            <Link to='intro' smooth={true} duration={500} className={styles.navButton}>INTRO</Link>
-            <Link to='about' smooth={true} duration={500} className={styles.navButton}>GET TO KNOW ME</Link>
-            <Link to='stack' smooth={true} duration={500} className={styles.navButton}>STACK</Link>
-            <Link to='projects' smooth={true} duration={500} className={styles.navButton}>MY PROJECTS</Link>
-            <Link to='contact' smooth={true} duration={500} className={styles.navButton}>GET IN TOUCH</Link>
-        </div>
-        <IoMenu className={styles.menuIcon } size={30}  onClick={toggleMenu}/>
-        
+      <p className={styles.navBarName}>Manau Tunjae</p>
+      
+      {/* Vi växlar mellan klasserna baserat på isOpen */}
+      <div className={`${styles.navBarContainer} ${isOpen ? styles.active : ""}`}>
+        <Link to="intro" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
+          INTRO
+        </Link>
+        <Link to="about" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
+          GET TO KNOW ME
+        </Link>
+        <Link to="stack" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
+          STACK
+        </Link>
+        <Link to="projects" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
+          MY PROJECTS
+        </Link>
+        <Link to="contact" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
+          GET IN TOUCH
+        </Link>
+      </div>
+
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        {isOpen ? <IoClose size={30} /> : <IoMenu size={30} />}
+      </div>
     </nav>
-    )
+  );
 }
 export default Navbar;
