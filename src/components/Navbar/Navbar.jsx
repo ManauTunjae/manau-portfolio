454d175d89c0eb42996268e11eac6b407ebc1a8e
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-18 17:40:03 +0200

    feat: update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,30 +17,70 @@ function Navbar() {
   };
 
   return (
-    <nav className={styles.navWrapper}>
-      {/* Vi växlar mellan klasserna baserat på isOpen */}
-      <div className={`${styles.navBarContainer} ${isOpen ? styles.active : ""}`}>
-        <Link to="intro" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
-          INTRO
-        </Link>
-        <Link to="about" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
-          GET TO KNOW ME
-        </Link>
-        <Link to="stack" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
-          STACK
-        </Link>
-        <Link to="projects" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
-          MY PROJECTS
-        </Link>
-        <Link to="contact" smooth={true} duration={500} className={styles.navLink} onClick={closeMenu}>
-          GET IN TOUCH
-        </Link>
-      </div>
+    <div>
+      <nav className={styles.navWrapper}>
+        {/* Vi växlar mellan klasserna baserat på isOpen */}
+        <img
+          src="/images/logo.svg"
+          alt="logo-imgae"
+          className={styles.logoImage}
+        />
+        <div
+          className={`${styles.navBarContainer} ${isOpen ? styles.active : ""}`}
+        >
 
-      <div className={styles.menuIcon} onClick={toggleMenu}>
-        {isOpen ? <IoClose size={30} /> : <IoMenu size={30} />}
-      </div>
-    </nav>
+          <Link
+            to="intro"
+            smooth={true}
+            duration={500}
+            className={styles.navLink}
+            onClick={closeMenu}
+          >
+            INTRO
+          </Link>
+          <Link
+            to="about"
+            smooth={true}
+            duration={500}
+            className={styles.navLink}
+            onClick={closeMenu}
+          >
+            GET TO KNOW ME
+          </Link>
+          <Link
+            to="stack"
+            smooth={true}
+            duration={500}
+            className={styles.navLink}
+            onClick={closeMenu}
+          >
+            STACK
+          </Link>
+          <Link
+            to="projects"
+            smooth={true}
+            duration={500}
+            className={styles.navLink}
+            onClick={closeMenu}
+          >
+            MY PROJECTS
+          </Link>
+          <Link
+            to="contact"
+            smooth={true}
+            duration={500}
+            className={styles.navLink}
+            onClick={closeMenu}
+          >
+            GET IN TOUCH
+          </Link>
+        </div>
+
+        <div className={styles.menuIcon} onClick={toggleMenu}>
+          {isOpen ? <IoClose size={30} /> : <IoMenu size={30} />}
+        </div>
+      </nav>
+    </div>
   );
 }
 export default Navbar;
