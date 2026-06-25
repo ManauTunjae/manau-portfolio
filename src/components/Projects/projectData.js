2aab6021f07d8ca797eff746b22e600e781e12b2
ManauTunjae <manau.tunjae@hotmail.com>
2026-06-25 11:05:52 +0200

    feat: add a project

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -3,6 +3,16 @@ export const myProjects = [
     id: 1,
     title: "mini-applicant tracking system",
     description:
+      "HireFlow är ett fullstack-baserat rekryteringssystem (Applicant Tracking System) utvecklat för att strömlinjeforma processen mellan jobbannonsering och kandidathantering. Systemet är byggt med ett strikt rollbaserat behörighetssystem (RBAC) där arbetsgivare (HR/Recruiters) och arbetssökande (Candidates) har helt separerade och skräddarsydda realtids-dashboards.",
+    tags: ["React", "Vite", "JavaScript", "CSS3"],
+    github: "https://github.com/ManauTunjae/HireFlow-FE.git", // Din länk
+    live: "https://www.hireflow.manautunjae.se", // Din live-länk
+    image: "/images/image-hireflowFE.png",
+  },
+  {
+    id: 2,
+    title: "mini-applicant tracking system",
+    description:
       "Mini-ATS är en modern fullstack-applikation byggd för att förenkla rekryteringsprocessen. Med fokus på hastighet, AI-insikter och ett rent användargränssnitt hjälper den rekryterare att hantera jobbannonser och kandidater effektivt.",
     tags: ["React", "Vite", "JavaScript", "CSS3"],
     github: "https://github.com/ManauTunjae/mini-applicant-tracking-system", // Din länk
@@ -10,7 +20,7 @@ export const myProjects = [
     image: "/images/Image-ats.png",
   },
   {
-    id: 2,
+    id: 3,
     title: "Professional Portfolio",
     description:
       "Min personliga utvecklarportfölj byggd med React. Funktioner inkluderar dynamisk projektrendering, anpassade CSS-moduler för en 'code-editor' estetik, och en helt responsiv layout.",
@@ -20,7 +30,7 @@ export const myProjects = [
     image: "/images/image-portfolio.png",
   },
   {
-    id: 3,
+    id: 4,
     title: "Quiz Application",
     description:
       "En kategoribaserad quiz-app med fokus på JSON-datahantering och DOM-manipulation för att skapa en sömlös användarupplevelse.",
@@ -30,7 +40,7 @@ export const myProjects = [
     image: "/images/image-quiz.png",
   },
   {
-    id: 4,
+    id: 5,
     title: "HireFlow ATS-application Backend",
     description:
       "Detta är ett säkert backend-system byggt för att strömna rekryteringsprocesser. Applikationen hanterar olika användarroller, datavalidering och molnbaserad bildhantering.",
@@ -40,7 +50,7 @@ export const myProjects = [
     image: "/images/image-hireflow.png",
   },
   {
-    id: 5,
+    id: 6,
     title: "Eventera",
     description:
       "Samarbetsprojekt med frontend-utvecklare som handlar om en plattform som sammanför olika evenemang från olika städer i Sverige. Backend byggd med Node.js, Express och MongoDB, med fokus på REST API-design och databasstrukturering.",
@@ -50,7 +60,7 @@ export const myProjects = [
     image: "/images/image-eventera.png",
   },
   {
-    id: 6,
+    id: 7,
     title: "Pokemon Application",
     description:
       "En interaktiv React-app som använder PokéAPI. Användare kan bläddra bland de ursprungliga 151 Pokémon och se detaljerad statistik och data.",
@@ -60,7 +70,7 @@ export const myProjects = [
     image: "/images/image-pokemon.png",
   },
   {
-    id: 7,
+    id: 8,
     title: "Dynamic Blog Platform",
     description:
       "En dynamisk bloggplattform med interaktiva inlägg. Demonstrerar array-datahantering, dynamisk DOM-rendering och en helt responsiv layout.",
@@ -70,7 +80,7 @@ export const myProjects = [
     image: "/images/image-blog.png",
   },
   {
-    id: 8,
+    id: 9,
     title: "Rhythm API",
     description:
       "Ett robust REST API för musikhantering. Har säker autentisering med JWT och fullständig MongoDB-databasintegration.",
